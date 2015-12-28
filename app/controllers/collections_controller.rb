a7b67bf5286df57ea80d33b36255b61d6b9727a5
Anastasiya Yevstyunicheva <ae@anahoret.com>
2015-12-28 18:01:21 +0200

    Added code.

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,19 +1,10 @@
 class CollectionsController < ActionController::Base
 
   def create
-    @service = AddFromWeb::Service.new(params[:url])
-    listing = @service.create_listing(@folio, @account)
+    @collection = Collections.new
+  end
 
-    if listing.errors.empty?
-      @folio.progress!(:listing_added)
-      @listing = FolioItemDecorator.new(listing)
-      @listing.created_by_current_user = true
-      render json: {
-        success:   render_to_string(partial: 'add_listing', layout: false),
-        listingId: listing.id
-      }
-    else
-      render :json => { :error => listing.errors.full_messages }
-    end
+  def show
+    @collection = Collections.all
   end
 end
